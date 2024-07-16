d27c3bea58683d8fd3435da7ccc83cebad2f4765
Márcio de Oliveira Dutra <78557074+marciodutra@users.noreply.github.com>
2024-07-16 18:08:18 -0300

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index']); // GET - http://127.0.0.1:8000/api/users?page=1
+Route::get('/users/{user}', [UserController::class, 'show']); // GET - http://127.0.0.1:8000/api/users/1
+Route::post('users', [UserController::class, 'store']); // POST - http://127.0.0.1:8000/api/users
+Route::put('users/{user}', [UserController::class, 'update']); // PUT - http://127.0.0.1:8000/api/users/1
